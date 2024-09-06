dee3ba22de99b0800ce9dab2b1ef8f3fe3ec88f4
faruzanrinney <anomalynumber16@gmail.com>
2024-09-05 23:52:54 -0700

    Add files via upload

---
+++ DifferenceObjectsClasses.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+class DifferenceObjectsClasses { // class name in which "DifferenceObjectsClasses" is the keyword
+	Object x = "apple"; // the string/object
+	public static void main(String[] args)
+    {
+		
+		DifferenceObjectsClasses fruit = new DifferenceObjectsClasses(); // creating the object for the class
+		
+		System.out.println(fruit.x); // printing out the object in the class
+	  }
+	}
